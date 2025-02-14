563670fa67fe7f00f43b7b3f64aca1b553b63c23
vipin018 <vipinpathak018@gmail.com>
2025-02-14 15:56:18 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import React from 'react'
 import { OrbitControls } from '@react-three/drei'
 // import * as THREE from 'three'
 import Cylinder from './Cylinder'
+import { Bloom, EffectComposer } from '@react-three/postprocessing'
 
-
-const App = () => {  
+const App = () => {
   return (
     <>
-    <Canvas >
-      <OrbitControls />
-      <ambientLight intensity={10} />
-      <Cylinder />
-    </Canvas>
+      <Canvas  >
+        <OrbitControls />
+        <ambientLight intensity={10} />
+        <Cylinder />
+        <EffectComposer>
+          <Bloom
+            mipmapBlur // Enables or disables mipmap blur.
+            intensity={1.0} // The bloom intensity.
+            luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+            luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+          // blurPass={undefined} // A blur pass.
+          // kernelSize={KernelSize.LARGE} // blur kernel size
+          // mipmapBlur={false} // Enables or disables mipmap blur.
+          // resolutionX={Resolution.AUTO_SIZE} // The horizontal resolution.
+          // resolutionY={Resolution.AUTO_SIZE} // The vertical resolution.
+          />
+        </EffectComposer>
+      </Canvas>
     </>
   )
 }
--- src/Cylinder.jsx
+++ src/Cylinder.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import { useTexture  } from '@react-three/drei'
 // import { CylinderGeometry, MeshStandardMaterial, Mesh } from 'three'
 import * as THREE from 'three'
-
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
 const Cylinder = () => {
 
-    let texture = useTexture('src/img3.jpg');
+    let texture = useTexture('src/img4.jpg');
+    let cyl = useRef(null);
+    useFrame((state, delta) => {
+        cyl.current.rotation.y += delta;
+    })
     return (
-        <mesh>
-            <cylinderGeometry args={[2, 2, 2, 30, 30, true]} />
+        <group ref={cyl}>
+        <mesh rotation={[0, 0.3, 0.3]} >
+            <cylinderGeometry args={[2, 2, 2, 60, 30, true]} />
             <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
         </mesh>
+        </group>
     )
 }
 
