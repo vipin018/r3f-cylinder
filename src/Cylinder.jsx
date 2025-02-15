7000864d44a67a5320feac73b34b6ce936e1f0c7
vipin018 <vipinpathak018@gmail.com>
2025-02-15 09:53:35 +0530

    updated

--- src/Cylinder.jsx
+++ src/Cylinder.jsx
@@ -6,17 +6,17 @@ import { useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 const Cylinder = () => {
 
-    let texture = useTexture('src/random.png');
+    let texture = useTexture('src/img.png');
     let cyl = useRef(null);
     useFrame((state, delta) => {
-        cyl.current.rotation.z += delta*1.5;
-        cyl.current.rotation.x += delta*2.5;
-        cyl.current.rotation.y += delta*1.;
+        // cyl.current.rotation.z += delta*1.5;
+        // cyl.current.rotation.x += delta*2.5;
+        cyl.current.rotation.y += delta*1.2;
     })
     return (
         <group ref={cyl}>
         <mesh rotation={[0, 0.2, 0.5]} >
-            <torusKnotGeometry args={[7,3 , 300, 20, 2, ]} />
+            <cylinderGeometry args={[2, 2, 2, 32, 1, true]} />
             <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
         </mesh>
         </group>
